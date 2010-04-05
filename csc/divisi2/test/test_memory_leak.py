a2bd605c36c85b5a675a34809ba522aa0cdadd4a
Rob Speer <rspeer@mit.edu>
2010-04-05 17:48:21 -0400

    working toward pickle support

--- csc/divisi2/test/test_memory_leak.py
+++ csc/divisi2/test/test_memory_leak.py
@@ -1,16 +1,8 @@
 from csc.conceptnet.analogyspace2 import *
 def test_memory_leak():
-    for w in xrange(20, 40):
-        print w
-        en = build_matrix('en', identity_weight=w)
-
-        U, S, V = en.normalize_all().svd(k=150)
-        #rec = divisi2.reconstruct(U, S, V)
-        #del en
-
-        #score = rec.evaluate_assertions('usertest_data.pickle')[-1]
-        #del rec
-        #print w, score
-        #print >> out, w, score
+    en = build_matrix('en', identity_weight=1)
+    for i in xrange(20):
+        print i
+        newen = en.squish()
 
 if __name__ == '__main__': test_memory_leak()
