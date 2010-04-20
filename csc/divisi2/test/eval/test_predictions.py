88934b26129f2a74a9784287b1bb6e7525ba0164
Rob Speer <rspeer@mit.edu>
2010-04-20 13:52:52 -0400

    make eval_predictions use the easier-to-load matrix file

--- csc/divisi2/test/eval/test_predictions.py
+++ csc/divisi2/test/eval/test_predictions.py
@@ -4,9 +4,7 @@ from nose.plugins.attrib import attr
 
 @attr('slow')
 def test_all_norm():
-    conceptnet = divisi2.load('data:graphs/conceptnet_en.graph')
-    matrix = divisi2.network.sparse_matrix(conceptnet, 'concepts', 'features',
-                                           cutoff=5)
+    matrix = divisi2.network.conceptnet_matrix('en')
     U, S, V = matrix.normalize_all().svd(k=100)
     rec = divisi2.reconstruct(U, S, V)
     correct, total, accuracy = rec.evaluate_assertions('data:eval/usertest_data.pickle')
