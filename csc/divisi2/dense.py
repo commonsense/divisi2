1f568bce41903ef71eaf4d66aeefedb2ed5db3f7
Rob Speer <rspeer@mit.edu>
2010-05-04 16:54:18 -0400

    Dense arrays can now take in labels and leave them alone. New row_norms and col_norms methods.

--- csc/divisi2/dense.py
+++ csc/divisi2/dense.py
@@ -92,6 +92,8 @@ class DenseVector(AbstractDenseArray, LabeledVectorMixin):
         obj = ndarray.view(cls)
         if labels is None:
             obj.labels = None
+        elif isinstance(labels, OrderedSet):
+            obj.labels = labels
         else:
             obj.labels = OrderedSet(labels)
         return obj
@@ -152,11 +154,17 @@ class DenseMatrix(AbstractDenseArray, LabeledMatrixMixin):
         obj = ndarray.view(cls)
         if row_labels is None:
             obj.row_labels = None
+        elif isinstance(row_labels, OrderedSet):
+            obj.row_labels = row_labels
         else:
+            print "converting rows to orderedset"
             obj.row_labels = OrderedSet(row_labels)
         if col_labels is None:
             obj.col_labels = None
+        elif isinstance(col_labels, OrderedSet):
+            obj.col_labels = col_labels
         else:
+            print "converting cols to orderedset"
             obj.col_labels = OrderedSet(col_labels)
         return obj
     
@@ -190,10 +198,20 @@ class DenseMatrix(AbstractDenseArray, LabeledMatrixMixin):
         norms = np.sqrt(np.sum(self*self, axis=1))[:, np.newaxis]
         return self / (norms + EPSILON)
 
+    def normalize_cols(self):
+        col_norms = self.col_norms()[np.newaxis, :]
+        return self / (col_norms + EPSILON)
+
     def normalize_all(self):
-        row_norms = np.sqrt(np.sum(self*self, axis=1))[:, np.newaxis]
-        col_norms = np.sqrt(np.sum(self*self, axis=0))[np.newaxis, :]
+        row_norms = self.row_norms()[:, np.newaxis]
+        col_norms = self.col_norms()[np.newaxis, :]
         return self / np.sqrt(row_norms + EPSILON) / np.sqrt(col_norms + EPSILON)
+    
+    def row_norms(self):
+        return np.sqrt(np.sum(self*self, axis=1))
+    
+    def col_norms(self):
+        return np.sqrt(np.sum(self*self, axis=0))
 
     @property
     def T(self):
