8772110a1c2a0307798775c21fd38ecebdaa41d7
Kenneth Arnold <kcarnold@alum.mit.edu>
2010-05-20 14:39:37 -0400

    Remove incremental SVD code for now.

--- svdlib/_svdlib.pyx
+++ svdlib/_svdlib.pyx
@@ -237,72 +237,6 @@ cdef class CSCMatrix:
         svdrec = svdLAS2A(self.cmatrix, k)
         return wrapSVDrec(svdrec, self.transposed)
                 
-    @cython.boundscheck(False) 
-    def isvd(self, int k=50, int niter=100, double lrate=.001):
-        from csc.divisi.tensor import DenseTensor
-        cdef smat* A = self.cmatrix
-        print "COMPUTING INCREMENTAL SVD"
-        print "ROWS: %d, COLUMNS: %d, VALS: %d" % (A.rows, A.cols, A.vals)
-        print "K: %d, LEARNING_RATE: %r, ITERATIONS: %d" % (k, lrate, niter)
-
-        cdef np.ndarray[DTYPE_t, ndim=2] u = np.add(np.zeros((A.rows, k), dtype=DTYPE), .001)
-        cdef np.ndarray[DTYPE_t, ndim=2] v = np.add(np.zeros((A.cols, k), dtype=DTYPE), .001)
-
-        # Maintain a cache of dot-products up to the current axis
-        cdef smat* predicted = svdNewSMat(A.rows, A.cols, A.vals)
-
-        # Type all loop vars
-        cdef unsigned int axis, i, cur_row,cur_col, col_index, next_col_index, value_index
-        cdef double err, u_value
-
-        # Initialize dot-product cache
-        # (This should be done with memcpy, but i'm not certain
-        # how to do that here)
-        for i in range(A.cols + 1):
-            predicted.pointr[i] = A.pointr[i]
-        
-        for i in range(A.vals):
-            predicted.rowind[i] = A.rowind[i]
-            predicted.value[i] = 0
-
-        for axis in range(k):
-            for i in range(niter):
-                # Iterate over all values of the sparse matrix
-                for cur_col in range(A.cols):
-                    col_index = A.pointr[cur_col]
-                    next_col_index = A.pointr[cur_col + 1]
-                    for value_index in range(col_index, next_col_index):
-                        cur_row = A.rowind[value_index]
-                        err = A.value[value_index] - (predicted.value[value_index] + 
-                                                      u[cur_row, axis] * v[cur_col, axis])
-
-                        u_value = u[cur_row, axis]
-                        u[cur_row, axis] += lrate * err * v[cur_col, axis]
-                        v[cur_col, axis] += lrate * err * u_value
-    
-            # Update cached dot-products
-            for cur_col in range(predicted.cols):
-                col_index = predicted.pointr[cur_col]
-                next_col_index = predicted.pointr[cur_col + 1]
-                for value_index in range(col_index, next_col_index):
-                    cur_row = predicted.rowind[value_index]
-                    predicted.value[value_index] += u[cur_row, axis] * v[cur_col, axis]
-
-        # Factor out the svals from u and v
-        u_sigma = np.sqrt(np.add.reduce(np.multiply(u, u)))
-        v_sigma = np.sqrt(np.add.reduce(np.multiply(v, v)))
-    
-        u_tensor = DenseTensor(np.divide(u, u_sigma))
-        v_tensor = DenseTensor(np.divide(v, v_sigma))
-        sigma = DenseTensor(np.multiply(u_sigma, v_sigma))
-
-        svdFreeSMat(predicted)
-
-        if self.transposed:
-            return v_tensor, u_tensor, sigma
-        else:
-            return u_tensor, v_tensor, sigma
-
 def svd(tensor, k=50, row_factors=None, offset_for_row=None, offset_for_col=None):
     CSC = CSCMatrix(tensor, offset_for_row, offset_for_col)
     CSC.pack(row_factors)
@@ -312,13 +246,3 @@ def dictSvd(tensor, k=50, row_factors=None, offset_for_row=None, offset_for_col=
     CSC = CSCMatrix(tensor, offset_for_row, offset_for_col)
     CSC.dictPack(row_factors)
     return CSC.svdA(k)
-
-def isvd(tensor, k=50, niter=100, lrate=.001, row_factors=None):
-    CSC = CSCMatrix(tensor)
-    CSC.pack(row_factors)
-    return CSC.isvd(k, niter, lrate)
-
-def dictIsvd(tensor, k=50, niter=100, lrate=.001, row_factors=None):
-    CSC = CSCMatrix(tensor)
-    CSC.dictPack(row_factors)
-    return CSC.isvd(k, niter, lrate)
