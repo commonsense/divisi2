8961ad2d8c376b5583838d22d593fd65f5ddd933
unknown <Rob Speer@.(none)>
2010-05-28 16:09:09 -0400

    check lengths of label lists

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ the results can include relationships not expressed in the original
 data but related by common sense. See http://divisi.media.mit.edu/ for
 more info."""
 
-VERSION = "2.0b3"
+VERSION = "2.0.0"
 
 try:
     from setuptools import setup, Extension
@@ -106,5 +106,5 @@ setup(
     packages=['csc', 'csc.divisi2'],
     package_data = {'csc.divisi2': ['data/graphs/*.graph', 'data/eval/*.pickle']},
     namespace_packages = ['csc'],
-    install_requires=['csc-utils >= 0.4.1', 'networkx'],
+    install_requires=['csc-utils >= 0.4.1', 'networkx', 'csc-pysparse'],
 )
