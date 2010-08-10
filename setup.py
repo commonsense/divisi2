e074dadc19f2662b8715cd22dab511e1e6582dc8
Rob Speer <rspeer@mit.edu>
2010-08-10 01:30:29 -0400

    v2.0.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ the results can include relationships not expressed in the original
 data but related by common sense. See http://divisi.media.mit.edu/ for
 more info."""
 
-VERSION = "2.0.4"
+VERSION = "2.0.5"
 
 try:
     from setuptools import setup, Extension
