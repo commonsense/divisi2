d600a534ccbc8529bc1b804191c009bcbdc60376
Catherine Havasi <havasi@media.mit.edu>
2010-09-21 12:15:20 -0400

    fix nmf

--- csc/divisi2/algorithms/nmf.py
+++ csc/divisi2/algorithms/nmf.py
@@ -1,5 +1,6 @@
 from py_nnma.nnma import FNMAI
 from csc.divisi2.dense import DenseMatrix
+import numpy as np
 
 def fnmai(matrix, k, U=None, V=None, **params):
     Y = matrix.to_scipy_csr()
@@ -7,7 +8,7 @@ def fnmai(matrix, k, U=None, V=None, **params):
     if V is not None: init_X = V.T
     else: init_X = None
     A, X, obj, count, converged = FNMAI(Y, k, A=init_A, X=init_X, **params)
-    sums = np.sum(A, axis=0) + np.sum(X, axis=0)
+    sums = np.sum(A, axis=0) + np.sum(X.T, axis=0)
     sum_order = np.argsort(sums)[::-1]
     U_out = A[:, sum_order]
     V_out = X.T[:, sum_order]
